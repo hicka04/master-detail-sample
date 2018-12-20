263abd3b68224373db3e949f9b6ff41b2ff4f5c9
hicka04 <calpissoda8221@gmail.com>
2018-12-20 11:00:50 +0900

    プロジェクト追加

---
+++ master-detail-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  master-detail-sample
+//
+//  Created by hicka04 on 2018/12/20.
+//  Copyright © 2018 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
